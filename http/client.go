fa8be259b5b74ea59c7c2362689dc86087cf9092
mlos100 <mel@mitre.org>
2016-02-02 07:33:31 -0500

    Add convenience function to invoke HTTP PUT

---
+++ http/client.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2016 The MITRE Corporation. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package http
+
+import (
+	"io"
+	"net/http"
+)
+
+// Put uses net/http DefaultClient to issue a PUT to the specified URL.
+//
+// Caller should close resp.Body when done reading from it.
+//
+// If the provided body is an io.Closer, it is closed after the
+// request.
+//
+// To set custom headers, use NewRequest and Client.Do.
+func Put(url string, bodyType string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", bodyType)
+	return http.DefaultClient.Do(req)
+}
